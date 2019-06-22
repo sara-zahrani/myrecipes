d4e21a116f8cceef56a0e4b393d27990947417cc
Sara Ali <sara.zahrani32@gmail.com>
2019-06-22 20:24:59 +0300

    Add a home page

---
+++ test/integration/pages_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class PagesTest < ActionDispatch::IntegrationTest
+
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+
+end
