a7b48b60b852271cc335738b83522b6ad3be6fbc
Sara Ali <sara.zahrani32@gmail.com>
2019-09-03 13:18:49 +0300

    Add a show action for recipe

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   get 'recipes', to: 'recipes#index'
 
+  get 'recipes/:id', to: 'recipes#show', as: 'recipe'
+
 end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -17,8 +17,18 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get recipes_url
     assert_template 'recipes/index'
-    assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_select "a[href=?]", recipe_url(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_url(@recipe2), text: @recipe2.name
+
   end
+
+  test "should get recipes show" do
+    get recipe_url(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
+  end
+
 
 end
